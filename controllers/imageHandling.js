0dc1268bc0888aa7e2aaad1b7b973a9760b5633c
Kevin Jake <kevin_jake_04@yahoo.com>
2023-03-09 21:08:54 +0800

    add renaming of image in s3 if it already exist

--- controllers/imageHandling.js
+++ controllers/imageHandling.js
@@ -2,13 +2,13 @@ import { uploadToS3 } from "../middleware/s3.js";
 
 export const uploadPhoto = async (req, res, next) => {
   const { file, user } = req;
-  console.log("🚀 ~ file: upload.js:5 ~ uploadPhoto ~ file:", file);
-
   if (!file) return res.status(400).json({ message: "Bad request" });
+  if (!user && !req.body.username)
+    return res
+      .status(400)
+      .json({ message: "Username is required. Before uploading a photo" });
 
-  const response = await uploadToS3({ file, user });
-  console.log("🚀 ~ file: upload.js:9 ~ uploadPhoto ~ response:", response);
-  // if (error) return res.status(500).json({ message: error.message });
+  const response = await uploadToS3({ file, user: user || req.body });
 
   return res.status(201).json(response);
 };
--- middleware/s3.js
+++ middleware/s3.js
@@ -1,4 +1,8 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  HeadObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -14,10 +18,46 @@ const config = {
 const s3 = new S3Client(config);
 
 export const uploadToS3 = async ({ file, user }) => {
-  const key = `${user?.username || "test"}(${user?.userId})/${
-    file.originalname
-  }`;
+  let key = `${user?.username}/${file.originalname}`;
   // const key = "test";
+
+  let suffix = "";
+  let objectExists = false;
+
+  try {
+    const headCommand = new HeadObjectCommand({
+      Bucket: BUCKET,
+      Key: key,
+    });
+
+    const result = await s3.send(headCommand);
+    objectExists = result ? true : false;
+  } catch (error) {}
+
+  if (objectExists) {
+    // If the object already exists in the bucket, add a numbered suffix to the filename
+    let count = 1;
+    const originalKey = key;
+    while (objectExists) {
+      const extension = originalKey.split(".").pop();
+      key = `${user?.username}/${
+        originalKey.split(".")[0].split("/")[1]
+      }-${count}.${extension}`;
+      try {
+        const headCommand = new HeadObjectCommand({
+          Bucket: BUCKET,
+          Key: key,
+        });
+
+        const result = await s3.send(headCommand);
+        objectExists = result ? true : false;
+        count += 1;
+      } catch (error) {
+        console.log(`Renaming file from ${file.originalname} to ${key}`);
+        objectExists = false;
+      }
+    }
+  }
 
   const uploadCommand = new PutObjectCommand({
     Bucket: BUCKET,
